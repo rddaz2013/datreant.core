8fcb8af1bc6263162262a888f14faa573ef45de1
rened <r.7dworschak@gmail.com>
2017-08-20 15:01:55 +0200

    pytest update2 without pep8

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setup(name='datreant.core',
       scripts=[],
       license='BSD',
       long_description=open('README.rst').read(),
-      tests_require = ['numpy', 'pytest>=2.10', 'mock'],
-      install_requires=['asciitree', 'pathlib2', 'scandir', 'six', 'fuzzywuzzy']
+      tests_require = ['numpy', 'pytest>=2.10'],
+      install_requires=['asciitree', 'pathlib2', 'six', 'fuzzywuzzy']
       )
